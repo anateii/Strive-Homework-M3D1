d6ec3494dead5fc05e9d1132c2ebcf2e178c5598
Ana Matei <anamatei.099@gmail.com>
2022-02-28 14:24:13 +0000

    Exercise 5 - Solved

--- M3D1.js
+++ M3D1.js
@@ -73,6 +73,16 @@ console.log(largestInt(14,7,21))
 Create a function to check whether two numbers are in range 40..60 or in the range 70..100 inclusive.
 */
 
+console.log("---EXERCISE 5---")
+
+function checkGivenNumbers (a,b) {
+    if ((a>=40 && a<=60 && b>=40 && b<=60) || (a>=70 && a<=100 && b>=70 && b<=100))
+    {return (true)}
+    else {return (false)}
+}
+
+console.log (checkGivenNumbers(46,56))
+
 /*
 6) 
 
